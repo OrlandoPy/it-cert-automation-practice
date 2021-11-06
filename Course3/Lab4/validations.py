51b5eb5b9a36804fde39eb8e689cbd9e594ca5a8
Orlando Gomez <orlipy5@gmail.com>
2021-11-06 16:45:06 +0000

    Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -22,6 +22,10 @@ def validate_user(username, minlen):
     if username[0] not in abc:
         return False
     return True
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
 
 
 
